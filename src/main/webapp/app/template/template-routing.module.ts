283a7624c7077c8a066858ff1edcd264c7216c23
Ngô Nhật Dương <nhatduong146@gmail.com>
2021-11-24 15:51:16 +0700

    modify detail-voucher.component

--- src/main/webapp/app/template/template-routing.module.ts
+++ src/main/webapp/app/template/template-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     },
   },
   {
-    path: 'detailVoucher',
+    path: 'voucher-detail',
     component: DetailVoucherComponent,
     data: {
       pageTitle: 'DetailVoucher',
