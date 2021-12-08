855317f03a80c29d018746c49000f341e4e6224c
VoXuanPhuc <phucpro746@gmail.com>
2021-12-08 13:55:56 +0700

    update template user module

---
+++ src/main/webapp/app/template/template-user/sildebar-user/sildebar-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-sildebar-user',
+  templateUrl: './sildebar-user.component.html',
+  styleUrls: ['./sildebar-user.component.scss'],
+})
+export class SildebarUserComponent implements OnInit {
+  //   constructor() { }
+
+  ngOnInit(): void {
+    return;
+  }
+}
---
+++ src/main/webapp/app/template/template-user/voucher-user/voucher-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-voucher-user',
+  templateUrl: './voucher-user.component.html',
+  styleUrls: ['./voucher-user.component.scss'],
+})
+export class VoucherUserComponent implements OnInit {
+  //   constructor() { }
+
+  ngOnInit(): void {
+    return;
+  }
+}
