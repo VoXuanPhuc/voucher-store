34e264264597b61715e56d4575f8c5fa7e8e438a
Ngô Nhật Dương <nhatduong146@gmail.com>
2021-12-12 15:53:37 +0700

    fix filter voucher

--- src/main/webapp/app/entities/service-type/voucher-service/voucher-service.component.ts
+++ src/main/webapp/app/entities/service-type/voucher-service/voucher-service.component.ts
@@ -4,44 +4,44 @@ import { IServiceType } from '../service-type.model';
 import { faFilm, faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'jhi-voucher-service',
-  templateUrl: './voucher-service.component.html',
-  styleUrls: ['./voucher-service.component.scss'],
+    selector: 'jhi-voucher-service',
+    templateUrl: './voucher-service.component.html',
+    styleUrls: ['./voucher-service.component.scss'],
 })
 export class VoucherServiceComponent implements OnInit {
-  serviceTypes?: IServiceType[];
-  isLoading = false;
-  selectedIndex?: number = -1;
-  typeId?: number;
+    serviceTypes?: IServiceType[];
+    isLoading = false;
+    selectedIndex?: number = -1;
+    typeId?: number;
 
-  @Output() typeChanged: EventEmitter<number> = new EventEmitter();
+    @Output() typeChanged: EventEmitter<number> = new EventEmitter();
 
-  constructor(private serviceTypeService: ServiceTypeService) {}
+    constructor(private serviceTypeService: ServiceTypeService) {}
 
-  ngOnInit(): void {
-    this.loadAll();
-  }
+    ngOnInit(): void {
+        this.loadAll();
+    }
 
-  setIndex(index: number): void {
-    this.selectedIndex = index;
-  }
+    setIndex(index: number): void {
+        this.selectedIndex = index;
+    }
 
-  trackId(index: number, item: IServiceType): number {
-    return item.id!;
-  }
+    trackId(index: number, item: IServiceType): number {
+        return item.id!;
+    }
 
-  public loadAll(): void {
-    this.serviceTypeService.query().subscribe(
-      res => {
-        this.isLoading = true;
-        this.serviceTypes = res.body ?? [];
-      },
-      error => window.console.log('An error occurred while loading data ', error)
-    );
-  }
+    public loadAll(): void {
+        this.serviceTypeService.query().subscribe(
+            res => {
+                this.isLoading = true;
+                this.serviceTypes = res.body ?? [];
+            },
+            error => window.console.log('An error occurred while loading data ', error)
+        );
+    }
 
-  onTypeChanged(id: number): void {
-    this.typeId = id;
-    this.typeChanged.emit(id);
-  }
+    onTypeChanged(id: number): void {
+        this.typeId = id;
+        this.typeChanged.emit(id);
+    }
 }
